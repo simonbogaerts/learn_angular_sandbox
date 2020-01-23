79d8221ca2cc6ae1679e952b3186788152fbd998
simonbogaerts <simon.bogaerts@outlook.com>
2020-01-23 14:24:28 +0100

    users base

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  constructor() {
-    console.log('ctr');
-  }
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+
+  constructor() {}
 
   ngOnInit() {
-    console.log('init');
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main St. ',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '21 Secondary St. ',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        age: 21,
+        address: {
+          street: '50 Main St. ',
+          city: 'Boston',
+          state: 'MA'
+        }
+      }
+    ];
+
+    this.loaded = true;
+
+    //this.showExtended = false;
+
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson'
+      // age: 22,
+      // address: {
+      //   street: '60 Wake St. ',
+      //   city: 'Boston',
+      //   state: 'MA'
+      // }
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 }
